03dbd28f1440fe8eff8622f95b1ff94a9f02a003
Ivan <contaivanrodrigues@gmail.com>
2025-07-02 15:43:20 -0300

    feat: add modal user

---
+++ server/src/models/User.ts
@@ -0,0 +1,11 @@
+import { Schema, model } from "mongoose";
+
+const userShema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+export const User = model("User", userShema);
