9a76a025f544912b9f5933dd6564e75f75d0d88f
Ivan <contaivanrodrigues@gmail.com>
2025-07-02 15:36:33 -0300

    feat: add conect mongoose

---
+++ server/src/database/index.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string)
+    console.log('🟢 Conectado ao MongoDB')
+  } catch (error) {
+    console.error('🔴 Erro ao conectar ao MongoDB:', error)
+  }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,20 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
+import { connectToDatabase } from "./database";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
+connectToDatabase();
 
-app.get('/',(req, res) => {
-    res.send('Servidor Express rodando com sucesso!')
-})
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor Express rodando com sucesso!");
+});
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`)
-})
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
