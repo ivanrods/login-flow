0274a791b673382ef6b55a01f42a941cec7568fd
Ivan <contaivanrodrigues@gmail.com>
2025-07-04 11:12:29 -0300

    feat: add login page

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { signIn } = useAuth();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signIn(email, password);
+    } catch (err: any) {
+      setError("Credenciais inválidas");
+    }
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type="submit">Entrar</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
