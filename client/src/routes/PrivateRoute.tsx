4c7250264ec7823afa177c6bdae1313b6f0a965e
Ivan <contaivanrodrigues@gmail.com>
2025-07-07 15:53:24 -0300

    fix: fix error build

--- client/src/routes/PrivateRoute.tsx
+++ client/src/routes/PrivateRoute.tsx
@@ -1,12 +1,14 @@
 import { Navigate } from "react-router-dom";
+import type { ReactNode } from "react";
 
 import { useAuth } from "../context/AuthContext";
 import { Profile } from "../pages/Profile";
 
-const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+const PrivateRoute = ({ children }: { children: ReactNode }) => {
   const { user } = useAuth();
   return user ? children : <Navigate to="/login" />;
 };
+
 
 function Private() {
   return (
